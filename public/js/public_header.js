0f0a3b21650e35188e272ad6fd0c65d03f174fb3
alinjiajun <alinjiajun@163.com>
2017-07-04 10:52:12 +0800

    public新建了header nav footer 的html模块template，css,js相关文件

--- public/js/public_header.js
+++ public/js/public_header.js
@@ -1,3 +1,27 @@
 /**
- * Created by Administrator on 2017/7/3.
+ * Created by jiajun on 2017/7/3.
  */
+;(function ($) {
+    $(function () {
+        //qq图标鼠标移入时显示文字提醒
+        showMsg('qq','qqkf');
+        //微信图标鼠标移入时显示二维码
+        showMsg('wx','codema');
+        //微博图标鼠标移入时显示信息
+        showMsg('sina','weibo');
+        function showMsg(id1,id2) {
+            var $target = $('#'+id1);
+            var $Content = $('#'+id2);
+            $target.hover(function () {
+                $Content.show();
+            },function () {
+                $Content.hide();
+            });
+            $Content.hover(function () {
+                $(this).show();
+            },function () {
+                $(this).hide();
+            });
+        }
+    })
+})(jQuery);
--- public/js/public_nav.js
+++ public/js/public_nav.js
@@ -1,3 +1,13 @@
 /**
  * Created by Administrator on 2017/7/3.
  */
+;(function ($) {
+    $('nav').on('mouseenter','.nav-li',function () {
+        if(!$(this).find('ul').is(':animated')){
+            $(this).children('ul').slideDown();
+        }
+    });
+    $('nav').on('mouseleave','.nav-li',function () {
+        $(this).children('ul').slideUp();
+    });
+})(jQuery);
